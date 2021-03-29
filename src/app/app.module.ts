1efaf666e8e3c3b88029219c45740615935d66b5
Baraka-Mugisha <mugishaje@gmail.com>
2021-03-29 17:52:30 +0200

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,7 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
